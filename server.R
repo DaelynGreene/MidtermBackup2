972832a7b324b57ed5f6d9258833348bd404adc4
Daelyn Greene <dgreen41@vols.utk.edu>
2022-03-23 13:31:02 -0400

    fixed the render so that it now updates based on actionbutton

--- server.R
+++ server.R
@@ -3,11 +3,14 @@ server <- function(input, output, session) {
   
   
   observeEvent(input$SearchLocation,{Website <- input$SearchLocation
-  
+
   if (Website == "FRED"){
-    renderUI({textInput("GTrend","What do you want to search for?")})
+    output$Selection <- renderUI({textInput("GTrend","What do you want to search for?")})
     
+  } else {
+    output$Selection <- NULL
   }
+  
   })
   
   
